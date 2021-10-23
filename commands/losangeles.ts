378909cf66402fc684a526f755fb601d0c1166c4
Jason Lo <github@jasonlo.email>
2021-10-22 20:57:50 -0700

    Wrong image

--- commands/losangeles.ts
+++ commands/losangeles.ts
@@ -2,6 +2,6 @@ import * as DJ from "discord.js";
 
 export function losangeles(bot: DJ.Client, msg: DJ.Message) {
   if (msg.content.toLowerCase().indexOf("/losangeles") >= 0) {
-    msg.channel.send(msg.author, { files: ["http://i.imgur.com/8TX82VJ.jpg"] });
+    msg.channel.send(msg.author, { files: ["http://i.imgur.com/SHBazSF.gif"] });
   }
 }
